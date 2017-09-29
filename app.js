99606d4a6f2d712895f22e8220cf9a1ab3765b50
Zhang LiTao <brucecham@qq.com>
2017-09-29 03:46:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 const Koa = require('koa')
+const router = require('koa-router')()
 const app = new Koa()
 
-// 记录执行的时间
-app.use(async (ctx, next)=>{
-  let stime = new Date().getTime()
-  await next()
-  let etime = new Date().getTime()
-  ctx.response.type = 'text/html'
-  ctx.response.body = '<h1>Hello World</h1>'
-  console.log(`请求地址: ${ctx.path}，响应时间：${etime - stime}ms`)
-});
-
-app.use(async (ctx, next) => {
-  console.log('中间件1 doSoming')
-  await next();
-  console.log('中间件1 end')
+// add url-route:
+router.get('/', async(ctx, next) => {
+  ctx.response.body = `<h1>index page</h1>`
 })
 
-app.use(async (ctx, next) => {
-  console.log('中间件2 doSoming')
-  await next();
-  console.log('中间件2 end')
+router.get('/home', async(ctx, next) => {
+  ctx.response.body = '<h1>HOME page</h1>'
 })
 
-app.use(async (ctx, next) => {
-  console.log('中间件3 doSoming')
-  await next();
-  console.log('中间件3 end')
+router.get('/404', async(ctx, next) => {
+  ctx.response.body = '<h1>404 Not Found</h1>'
 })
+
+// add router middleware:
+app.use(router.routes())
 
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000')
