e454605faf50075ba5d6a9dc19fc6487d7c6b95f
brucecham <brucecham@qq.com>
2017-10-18 11:40:54 +0800

    add code

---
+++ koa2-tutorial/app.js
@@ -0,0 +1,13 @@
+const Koa = require('koa')
+const app = new Koa()
+
+// 增加代码
+app.use(async (ctx, next) => {
+  await next()
+  ctx.response.type = 'text/html'
+  ctx.response.body = '<h1>Hello World</h1>'
+})
+
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000')
+})
