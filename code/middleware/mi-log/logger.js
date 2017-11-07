e1d9a28443c83bb0370d089aa3a17edc9d511c61
Zhang LiTao <brucecham@qq.com>
2017-11-07 18:41:06 +0800

    Update logger.js

--- code/middleware/mi-log/logger.js
+++ code/middleware/mi-log/logger.js
@@ -1,9 +1,7 @@
 const log4js = require('log4js');
-const path = require("path");
 const access = require("./access.js");
 const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
 
-// 提取默认公用参数对象
 const baseInfo = {
   appLogLevel: 'debug',
   dir: 'logs',
@@ -12,14 +10,12 @@ const baseInfo = {
   serverIp: '0.0.0.0'
 }
 
-// 提取变量为参数对象
 module.exports = ( options ) => {
   let contextLogger = {} 
   let config = {}
 
-  // 继承自 baseInfo 默认参数
   const opts = Object.assign({}, baseInfo, options || {}) ;
-  const {projectName,serverIp,appLogLevel,dir,env} = opts;
+  const { env, appLogLevel, dir, serverIp, projectName } = opts
   
   const currentLevel = methods.findIndex(ele => ele === appLogLevel)
   const appenders = {}
@@ -46,30 +42,23 @@ module.exports = ( options ) => {
     }
   }
 
-  const accessLogger = log4js.getLogger("access");
-  
-  const performanceLogger = log4js.getLogger("performance")
+  const logger = log4js.getLogger('cheese');
 
   return async (ctx, next) => {
     const start = Date.now()
 
     log4js.configure(config)
     methods.forEach((method, i) => {
-      if (i >= currentLevel) {
-        contextLogger[method] = (message) => {
-          performanceLogger[method](access(ctx, message, commonInfo))
-        }
-      } else {
-        contextLogger[method] = () => {}
+      contextLogger[method] = (message) => {
+        logger[method](access(ctx, message, commonInfo))
       }
     })
     ctx.log = contextLogger;
 
-    const logger = log4js.getLogger()
     await next()
-    const delta = Date.now() - start;
-    accessLogger.info(access(ctx, {
-      responseTime: delta
+    const responseTime = Date.now() - start;
+    logger.info(access(ctx, {
+      responseTime: `响应时间为${responseTime/1000}s`
     }, commonInfo))
   }
 }
