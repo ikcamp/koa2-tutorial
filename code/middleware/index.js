c997c5087e30a71eed9306968714768a87e76f79
Zhang LiTao <brucecham@qq.com>
2017-12-04 10:16:17 +0800

    Update index.js

--- code/middleware/index.js
+++ code/middleware/index.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
     errorPageFolder: path.resolve(__dirname, '../errorPage')
   }))
 
-  app.use(miLog(app.env, {
+  app.use(miLog({
     env: app.env,
     projectName: 'koa2-tutorial',
     appLogLevel: 'debug',
